41d4fa46f2bc3a7b238445cfa0716ea82605491e
Vinicius <v.a.beraldo@gmail.com>
2021-11-20 12:11:54 -0300

    Actuator info
    adição de informações no endpoint "info" do actuator

---
+++ Estudo-Java/Projeto_SpringF/bookstoremanager/src/main/java/com/vinidev/bookstoremanager/custominfoendpoint/InfoWebPointExtension.java
@@ -0,0 +1,31 @@
+
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.web.WebEndpointResponse;
+import org.springframework.boot.actuate.endpoint.web.annotation.EndpointWebExtension;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@EndpointWebExtension(endpoint = InfoEndpoint.class)
+public class InfoWebPointExtension {
+
+    @Autowired
+    private InfoWebPoint delegate;
+    
+    @ReadOperation
+    public WebEndpointResponse<Map<String, Object>> info() {
+        Map<String, Object> info = this.delegate.info();
+        Integer status = getStatus(info);
+        Map<String, Object> customInfo = new HashMap<>(info);
+        customInfo.put("customInfo","handOn");
+        return new WebEndpointResponse<>(customInfo, status);
+    }
+
+    private Integer getStatus(Map<String, Object> info){
+        return 200;
+    } 
+    
+    
+}
