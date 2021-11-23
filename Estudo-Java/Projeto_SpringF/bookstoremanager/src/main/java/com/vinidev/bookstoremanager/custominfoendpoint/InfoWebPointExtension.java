d3f2797ce4992451675b7b26b716000657753096
Vinicius <v.a.beraldo@gmail.com>
2021-11-23 08:56:23 -0300

    Added system.properties

--- Estudo-Java/Projeto_SpringF/bookstoremanager/src/main/java/com/vinidev/bookstoremanager/custominfoendpoint/InfoWebPointExtension.java
+++ Estudo-Java/Projeto_SpringF/bookstoremanager/src/main/java/com/vinidev/bookstoremanager/custominfoendpoint/InfoWebPointExtension.java
@@ -1,9 +1,11 @@
 
+import java.util.HashMap;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 import org.springframework.boot.actuate.endpoint.web.WebEndpointResponse;
 import org.springframework.boot.actuate.endpoint.web.annotation.EndpointWebExtension;
+import org.springframework.boot.actuate.info.InfoEndpoint;
 import org.springframework.stereotype.Component;
 
 
@@ -12,10 +14,10 @@ import org.springframework.stereotype.Component;
 public class InfoWebPointExtension {
 
     @Autowired
-    private InfoWebPoint delegate;
+    private InfoEndpoint delegate;
     
     @ReadOperation
-    public WebEndpointResponse<Map<String, Object>> info() {
+    public WebEndpointResponse<Map> info() {
         Map<String, Object> info = this.delegate.info();
         Integer status = getStatus(info);
         Map<String, Object> customInfo = new HashMap<>(info);
