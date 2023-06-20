5b74bf3bc528b9fb9bed79554300d47f6f7c0167
viniberaldo <v.a.beraldo@gmail.com>
2023-06-20 18:28:02 -0300

    introdução da biblioteca thymeleaf

--- Estudo-Java/screenmatch/src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
+++ Estudo-Java/screenmatch/src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
@@ -5,6 +5,7 @@ import br.com.alura.screenmatch.domain.filme.Filme;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,15 +16,21 @@ public class FilmeController {
 
     private List<Filme> filmes = new ArrayList<>();
 
-    @GetMapping
+    @GetMapping("/formulario")
     public String carregarPaginaFormulario() {
         return "filmes/formulario";
+    }
+
+    @GetMapping
+    public String carregarPaginaListagem(Model model) {
+        model.addAttribute("lista", filmes);
+        return "filmes/listagem";
     }
 
     @PostMapping
     public String cadastrarFilme(DadosCadastroFilme dados) {
         Filme filme = new Filme(dados);
         filmes.add(filme);
-        return "filmes/formulario";
+        return "redirect:/filmes";
     }
 }
