086e16fa233476c5273278dd2877cbb22fd6250f
viniberaldo <v.a.beraldo@gmail.com>
2023-09-23 11:21:33 -0300

    primeiro commit

---
+++ Estudo-Java/projeto_spring_boot/exercicio-spring/src/test/java/br/com/dev/exerciciospring/ExercicioSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dev.exerciciospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
